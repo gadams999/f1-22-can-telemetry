c4b7367dded152c32eca3645b0b62b526bf27b0a
Gavin Adams <gavinaws@amazon.com>
2022-11-11 16:12:05 -0700

    start of socket can

--- f1telem/go.mod
+++ f1telem/go.mod
@@ -1,3 +1,15 @@
 module github.com/gadams999/f1telem
 
-go 1.19
+go 1.18
+
+require go.einride.tech/can v0.4.0
+
+require (
+	github.com/golang/mock v1.6.0 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220106191415-9b9b3d81d5e3 // indirect
+	golang.org/x/net v0.0.0-20220923203811-8be639271d50 // indirect
+	golang.org/x/sync v0.0.0-20220923202941-7f9b1623fab7 // indirect
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+	golang.org/x/tools v0.1.10 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
--- f1telem/main.go
+++ f1telem/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -9,6 +10,8 @@ import (
 	"syscall"
 
 	"github.com/gadams999/f1telem/packet"
+	"go.einride.tech/can"
+	"go.einride.tech/can/pkg/socketcan"
 )
 
 func main() {
@@ -23,13 +26,19 @@ func main() {
 
 		s, err := net.ResolveUDPAddr("udp4", PORT)
 		if err != nil {
-			fmt.Println(err)
+			log.Println(err)
 			return
 		}
 
 		connection, err := net.ListenUDP("udp4", s)
 		if err != nil {
-			fmt.Println(err)
+			log.Println(err)
+			return
+		}
+
+		canCon, err := socketcan.DialContext(context.Background(), "can", "vcan0")
+		if err != nil {
+			log.Println(err)
 			return
 		}
 
@@ -40,7 +49,7 @@ func main() {
 			// Read  UDP packet into buffer
 			n, addr, err := connection.ReadFromUDP(buffer)
 			if err != nil {
-				fmt.Print("read udp failed", n, addr, err)
+				log.Println("read udp failed", n, addr, err)
 				continue
 			}
 
@@ -49,15 +58,15 @@ func main() {
 			if eventType == 6 {
 				telemetryData, err := packet.TelemetryPacket(buffer)
 				if err != nil {
-					fmt.Print("Error parsing telemetry packet ", err)
+					log.Println("Error parsing telemetry packet ", err)
 				}
 
 				// Read the player's car telemetry
 				playerData, err := packet.PlayerData(telemetryData)
 				if err != nil {
-					fmt.Print("Error getting player data ", err)
+					log.Println("Error getting player data ", err)
 				}
-				SendToCan(playerData)
+				SendToCan(playerData, canCon)
 			}
 		}
 	}()
@@ -66,9 +75,10 @@ func main() {
 	log.Print("Stopping f1telem service")
 }
 
-func SendToCan(telem packet.PlayerTelemetryData) {
+func SendToCan(telem packet.PlayerTelemetryData, conn net.Conn) {
 	var speed_obd2 uint8
 	var speed_custom uint16
+	var frame can.Frame
 
 	if telem.CarTelemetryData.M_speed <= 255 {
 		speed_obd2 = uint8(telem.CarTelemetryData.M_speed)
@@ -77,4 +87,12 @@ func SendToCan(telem packet.PlayerTelemetryData) {
 	}
 	speed_custom = telem.CarTelemetryData.M_speed
 	fmt.Printf("speed (obd2 - custom), %d - %d\n", speed_obd2, speed_custom)
+
+	frame.ID = 0xd
+	frame.Length = 1
+	frame.Data.SetUnsignedBitsLittleEndian(0, 1, uint64(speed_obd2))
+
+	tx := socketcan.NewTransmitter(conn)
+	_ = tx.TransmitFrame(context.Background(), frame)
+
 }
